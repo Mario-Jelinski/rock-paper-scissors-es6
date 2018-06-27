8d160cb369eee2bb6de49d477c8f8a67a41d3dc7
mjelinski <jelinski@strato.de>
2018-06-27 13:43:53 +0200

    eslint added

--- assets_include.mk
+++ assets_include.mk
@@ -15,12 +15,12 @@ styles:
 build:
 	eslint js/game.js js/computer.js js/player.js js/solver.js
 	mkdir -p public/js
-	browserify -o public/js/game.js js/game.js --debug --transform [ babelify --presets [ es2017 ] --plugins [ babel-plugin-transform-runtime babel-plugin-transform-async-to-generator ] ] --plugin browserify-derequire
-	uglifyjs -c -m -o js/game.js public/js/game_es6.js --source-map "root='/',url='$(@F).map'"
-	uglifyjs -c -m -o js/computer.js public/js/computer.js --source-map "root='/',url='$(@F).map'"
-	uglifyjs -c -m -o js/player.js public/js/player.js --source-map "root='/',url='$(@F).map'"
-	uglifyjs -c -m -o js/solver.js public/js/solver.js --source-map "root='/',url='$(@F).map'"
-	uglifyjs -c -m -o public/js/game.js public/js/game.js --source-map "root='/',url='$(@F).map'"
+	browserify -o public/js/game.js js/game.js --debug --transform [ babelify --presets [ es2015 ] --plugins [ babel-plugin-transform-runtime babel-plugin-transform-async-to-generator ] ] --plugin browserify-derequire
+#	uglifyjs -c -m -o public/js/game_es6.js js/game.js --source-map "root='/',url='$(@F).map'"
+#	uglifyjs -c -m -o public/js/computer.js js/computer.js --source-map "root='/',url='computer.map'"
+#	uglifyjs -c -m -o public/js/player.js js/player.js --source-map "root='/',url='player.map'"
+#	uglifyjs -c -m -o public/js/solver.js js/solver.js --source-map "root='/',url='solver.map'"
+#	uglifyjs -c -m -o public/js/game.js public/js/game.js --source-map "root='/',url='game.map'"
 
 clean:
 	rm -vrf public/js
--- js/computer.js
+++ js/computer.js
@@ -1,4 +1,5 @@
 /*global Player, module */
+/* exported Player */
 
 let Player = require('./player.js');
 
